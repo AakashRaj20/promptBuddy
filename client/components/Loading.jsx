46c52e166e4a558c52366d7fc520d2b6c4bfc0f3
Aakash Raj <aakashraj914@gmail.com>
2023-10-06 16:20:23 +0530

    fixed Loader component render issue in other user profile page

--- client/components/Loading.jsx
+++ client/components/Loading.jsx
@@ -1,14 +1,19 @@
+"use client"
+
+import { useEffect } from "react";
 import Image from "next/image";
+import { useSearchParams, usePathname } from "next/navigation";
 
 const Loading = () => {
+ 
   return (
-    <div className='w-full flex-center'>
+    <div className="w-full flex-center">
       <Image
-        src='assets/icons/loader.svg'
+        src="/assets/icons/loader.svg"
         width={50}
         height={50}
-        alt='loader'
-        className='object-contain'
+        alt="loader"
+        className="object-contain"
       />
     </div>
   );
