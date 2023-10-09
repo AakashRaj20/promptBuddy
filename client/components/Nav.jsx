3cff17a525258e8b8b905c081579a62fce9cb69b
Aakash Raj <aakashraj914@gmail.com>
2023-10-09 16:34:53 +0530

    fixed position for dark_mode icon

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <div className="flex">
+      <div className="flex gap-11">
         <Link href="/" className="flex gap-2 flex-center">
           <Image
             src="/assets/images/logo.png"
--- client/components/ThemeSwitcher.jsx
+++ client/components/ThemeSwitcher.jsx
@@ -33,10 +33,3 @@ const ThemeSwitch = () => {
 
 export default ThemeSwitch;
 
-{
-  /* <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select> */
-}
