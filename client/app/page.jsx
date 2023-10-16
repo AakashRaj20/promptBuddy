e04d5b67c412551a039cadd43faa8eafbb2fcce3
Aakash Raj <aakashraj914@gmail.com>
2023-10-16 20:28:54 +0530

    fixed ui for desktop view

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,17 +1,12 @@
 import Feed from "@components/Feed";
 
 const Home = () => (
-  <section className='w-full flex-center flex-col'>
-    <h1 className='head_text text-center'>
+  <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
       Discover & Share
-      <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center"> AI-Powered Prompts</span>
     </h1>
-    <p className='desc text-center'>
-      PromptBuddy is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts, to help people use AI better.
-    </p>
-
     <Feed />
   </section>
 );
